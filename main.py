34f06334aa92494a645d0fd56e498c484d62fde0
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2021-05-26 02:44:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from biopandas.mol2 import PandasMol2
 from biopandas.mol2 import split_multimol2
 
 class molecule:
-    def __init__(self,mol2_id: str,mol2_cont: str):
-        self.mol2_id = mol2_id
+    def __init__(self,mol2_cont: str):
         self.mol2_cont = mol2_cont
     def show(self):
+        print(self.mol2_cont)
         for each in self.mol2_cont:
             for e in each:
-                print(e[:-1])
+                if e[-1] == '\n':
+                    print(e[:-1])
+                else: print(e)
+
 
     def parser(Path):
         with open(Path, 'r') as f:
@@ -23,8 +26,9 @@ class molecule:
             for mol2 in split_multimol2(Path):
                 count += 1
                 Call = "molecule " + str(count)
-                mol2_id = mol2[0]
+                #mol2_id = mol2[0]
                 mol2_cont = mol2[1:]
-                Call = molecule(mol2_id, mol2_cont)
+                Call = molecule(mol2_cont)
                 Array.append(Call)
+            return Array
             return Array
