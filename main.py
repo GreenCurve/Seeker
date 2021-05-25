074bbcbcca04cd6ba212a3386ee36f48eec4524c
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2021-05-26 02:24:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,11 @@ import configparser
 from biopandas.mol2 import PandasMol2
 from biopandas.mol2 import split_multimol2
 
-Path = input("Введите путь к файлу: ") #C:\Users\Egor\Repo\input\ligands\mol2_F.mol2
-Path = r"C:\Users\Egor\Repo\input\ligands\mol2_F.mol2" if Path == "" else Path
-print(Path)
-#pmol = PandasMol2().read_mol2(Path)
-#Tsar = pmol.df[pmol.df['atom_type'] == 'C.ar']
-#print('Molecule ID: %s' % pmol.code)
-#print('\nRaw MOL2 file contents:\n\n%s\n...' % pmol.mol2_text)
-#print('number of Russian Tsars: %d' % Tsar.shape[0])
-with open(Path, 'r') as f:
-    for mol2 in split_multimol2(Path):
-        mol2_id = mol2[0]
-        mol2_cont = mol2[1:]
-        print('Molecule ID:\n', mol2_id)
-        print('Content:\n')
-        for each in mol2_cont:
+class molecule:
+    def __init__(self,mol2_id: str,mol2_cont: str):
+        self.mol2_id = mol2_id
+        self.mol2_cont = mol2_cont
+    def show(self):
+        for each in self.mol2_cont:
             for e in each:
                 print(e[:-1])
