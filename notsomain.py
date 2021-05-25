cd25a03d577755fb737a8947e9fb086acfe19fce
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2021-05-26 02:36:13 +0300

    Update notsomain.py

--- notsomain.py
+++ notsomain.py
@@ -9,11 +9,6 @@ from biopandas.mol2 import split_multimol2
 from main import molecule
 
 Path = input("Введите путь к файлу: ") #C:\Users\Egor\Repo\input\ligands\mol2_F.mol2
-Number = input("какой номер хочешь посмотреть?")
+Number = int(input("какой номер хочешь посмотреть? "))
 Path = r"C:\Users\Egor\Repo\input\ligands\mol2_F.mol2" if Path == "" else Path
-def parser(Path):
-    with open(Path, 'r') as f:
-        for mol2 in split_multimol2(Path):
-            mol2_id = mol2[0]
-            mol2_cont = mol2[1:]
-            molecule = molecule(mol2_id,mol2_cont)
+print(molecule.parser(Path)[Number].show())
