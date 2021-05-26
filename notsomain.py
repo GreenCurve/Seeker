2b39e747be6c94b7fa663560e661be9e631fbefb
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2021-05-26 21:02:21 +0300

    Update notsomain.py

--- notsomain.py
+++ notsomain.py
@@ -11,4 +11,7 @@ from main import molecule
 Path = input("Введите путь к файлу: ") #C:\Users\Egor\Repo\input\ligands\mol2_F.mol2
 Number = int(input("какой номер хочешь посмотреть? "))
 Path = r"C:\Users\Egor\Repo\input\ligands\mol2_F.mol2" if Path == "" else Path
-print(molecule.parser(Path)[Number].show())
+#print(molecule.parser(Path)[Number].show())
+Map = molecule.parser(Path)[Number].show()
+print(Map)
+molecule.mapper(Map[2])
